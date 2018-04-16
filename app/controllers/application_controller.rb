793d9786c87682379516de60d3f1828b789903e9
tatematsu-k <ynu1264@gmail.com>
2018-04-16 18:46:43 +0900

    不要なコントローラーの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :init, :authenticate_user!
   protect_from_forgery with: :exception
 
-  # def login_check
-  #   p 'call login check'
-  #   if (user = session[:login_user])
-  #     @login_user = User.find(user)
-  #     unless @login_user
-  #       puts "access user is not exist in DB (login_id: #{user} )"
-  #       redirect_to login_path
-  #     end
-  #
-  #     init
-  #   else
-  #     flash[:error_message] = 'ログイン後一定時間操作が無かったため自動ログアウトしました。'
-  #     puts 'fails access to not find login session'
-  #     redirect_to login_path
-  #   end
-  # end
-
   def init
     set_menu_items
   end
@@ -48,10 +31,5 @@ class ApplicationController < ActionController::Base
     @menu_items.each do |_key, item|
       item[:active] = false
     end
-  end
-
-  # check login status
-  def login_check_enable?
-    true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,5 @@ Rails.application.routes.draw do
   end
 
   resources :notes
-  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

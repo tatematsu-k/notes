08ab2af741633e5ebbcfa1a2a0c25f9936b09e4d
tatematsu-k <ynu1264@gmail.com>
2018-04-30 22:02:26 +0900

    noteのヘルパー化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def basic_card(options = {}, &block)
+    options = {
+      class: 'card',
+      style: 'max-width: 20rem;'
+    }.merge(options)
+    content_tag(:div, capture(&block), options)
+  end
+
+  def card_header(&block)
+    content_tag(:div, capture(&block), class: 'card-header')
+  end
+
+  def card_body(&block)
+    content_tag(:div, class: 'card-body') do
+      concat content_tag(:p, capture(&block), class: 'card-text')
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Notes
 
     config.i18n.default_locale = :ja
 
+    config.action_controller.include_all_helpers = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

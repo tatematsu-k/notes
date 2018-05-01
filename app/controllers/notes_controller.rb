553010d8e008d452610ff8e429b11936aa8c7f74
tatematsu-k <ynu1264@gmail.com>
2018-05-01 09:27:54 +0900

    タグが付いているノートを削除するとエラーが出る問題の修正

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -60,6 +60,7 @@ class NotesController < ApplicationController
   # DELETE /notes/1
   # DELETE /notes/1.json
   def destroy
+    TagMemo.delete_by_note_id(@note.id)
     @note.destroy
     respond_to do |format|
       format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
